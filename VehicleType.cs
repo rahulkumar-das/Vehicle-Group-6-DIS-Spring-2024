a52c1ffc1cd74631554ac38809472ad5070c3cb4
ankita3097 <ankita.swork3097@gmail.com>
2024-03-06 12:12:54 -0500

    Vehicle Types Code Written.

---
+++ VehicleType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Group_6_DIS_Spring_2024
+{
+    internal class VehicleType
+    {
+        public string model;
+        public string type;
+
+
+        // Getter method for Model
+        public string GetModel()
+        {
+            return model;
+        }
+
+        // Getter method for Type
+        public string getType()
+        {
+            return type;
+        }
+
+        public void setModel(string model)
+        {
+            model = this.model;
+        }
+
+        public void setType(string type)
+        {
+            type = this.type;
+        }
+
+        // Method to display vehicle information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Vehicle Model: {model}");
+            Console.WriteLine($"Vehicle Type: {type}");
+        }
+    }
+}
