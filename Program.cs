fd936fb4ffffc63e5971e1e7ef88990059fc7107
Rahul <rahuldk101@gmail.com>
2024-03-06 11:35:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vehicle_Group_6_DIS_Spring_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
